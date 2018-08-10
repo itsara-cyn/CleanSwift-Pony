db1e7532fb3e9ce7ed8567abf4bb2fcb78ea2c62
Itsaraporn Chaichayanon <jern@appsynth.net>
2018-08-10 14:09:14 +0700

    Remove Clean-Swift header comment line, Create configulator template.

---
+++ Clean Swift/Scene.xctemplate/UIViewController/___FILEBASENAME___Configurator.swift
@@ -0,0 +1,27 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+
+class ___VARIABLE_sceneName___Configurator {
+    
+    static let sharedInstance = ___VARIABLE_sceneName___Configurator()
+    
+    private init() {}
+    
+    func configure(viewController: ___VARIABLE_sceneName___ViewController) {
+        let interactor = ___VARIABLE_sceneName___Interactor()
+        let presenter = ___VARIABLE_sceneName___Presenter()
+        let router = ___VARIABLE_sceneName___Router()
+        viewController.interactor = interactor
+        viewController.router = router
+        interactor.presenter = presenter
+        presenter.viewController = viewController
+        router.viewController = viewController
+        router.dataStore = interactor
+    }
+}
